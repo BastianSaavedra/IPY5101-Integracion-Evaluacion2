7297699eba9ec93da62c17c59093409f9e45df53
Zets <bastiansaavedra55@gmail.com>
2021-06-06 21:08:31 -0700

    php init

---
+++ webpay/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7779a93bdfef1b6bb4ace571ee17f42dd5a418de',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7779a93bdfef1b6bb4ace571ee17f42dd5a418de',
+            'dev_requirement' => false,
+        ),
+        'freshwork/transbank' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freshwork/transbank',
+            'aliases' => array(),
+            'reference' => '690e15bd6cdeb6897f49ab12b18c3d48362e8d50',
+            'dev_requirement' => false,
+        ),
+    ),
+);
