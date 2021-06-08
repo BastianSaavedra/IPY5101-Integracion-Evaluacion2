be770c0932cd84a39f241ac1754e82acc8d2c9a1
Bastian Saavedra <69812594+KizsawaQwerty@users.noreply.github.com>
2021-06-08 00:10:22 -0400

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,5 +1,7 @@
 <?php
 
+// Codigo para creacion de usuarios para testeo
+
 //curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer TEST-7651103485962948-060722-1820322e0c6aa077a12a5c41a6879504-215043503" "https://api.mercadopago.com/users/test_user" -d "{'site_id':'MLC'}"
 
 
@@ -9,12 +11,27 @@
 // VENDEDOR
 //{"id":772018385,"nickname":"TESTPUUFXR9A","password":"qatest3667","site_status":"active","email":"test_user_6995951@testuser.com"}
 
+// CLIENTE
+//{"id":772140151,"nickname":"TETE6397533","password":"qatest946","site_status":"active","email":"test_user_18205845@testuser.com"}
+
+// VENDEDOR
+//{"id":772141663,"nickname":"TETE5998845","password":"qatest6085","site_status":"active","email":"test_user_69432462@testuser.com"}
 
 // DATOS TARJETA DE PRUEBA
 /**
  * Visa
  * 4168 8188 4444 7115
  * 123
+ * 11/25
+ * 
+ * Mastercard
+ * 5416 7526 0258 2580
+ * 123
+ * 11/25
+ * 
+ * American Express
+ * 3757 781744 61804
+ * 1234
  * 11/25
  */
 
@@ -23,7 +40,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('TEST-6148407871143424-060800-c5b4fda15bc70444d4b11da476ab1f39-772009608');
+MercadoPago\SDK::setAccessToken('TEST-8506700935294904-060803-2938c25050acdfad31f7156224742b12-772140151');
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
@@ -39,9 +56,9 @@ $preference->auto_return = "approved";
 $datos=array();
 for($x=0;$x<10;$x++){
     $item = new MercadoPago\Item();
-    $item->title = 'Serranita';
-    $item->quantity = 2;
-    $item->unit_price = 500;
+    $item->title = 'Product Test';
+    $item->quantity = 1;
+    $item->unit_price = 1000;
     $datos[]=$item;
 }
 
@@ -60,8 +77,7 @@ $preference->save();
     </head>
     <body>
         <form action="http://localhost/IPY5101-Integracion-Evaluacion2/insertarpago.php" method="POST">
-            <h1>Producto:</h1>
-            <h2>Serranitas</h2>
+            <h2>Product Test</h2>
             <script 
                 src="https://www.mercadopago.cl/integrations/v1/web-payment-checkout.js"
                 data-preference-id="<?php echo $preference->id; ?>">
